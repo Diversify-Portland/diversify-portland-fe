bfbacbe6c2bb53d937027c84b72fa6e401d4a275
chrispiccaro <chrispiccaro18@gmail.com>
2020-08-23 14:27:04 -0700

    Buttons

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
-import LogoutButton from './buttons/LogoutButton';
-import LoginButton from './buttons/LoginButton';
+import LogoutButton from './Buttons/LogoutButton';
+import LoginButton from './Buttons/LoginButton';
 
 const Login = () => {
   const { isAuthenticated } = useAuth0();
