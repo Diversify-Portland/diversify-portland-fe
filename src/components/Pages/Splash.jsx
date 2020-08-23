0a167da6227d109d4636f411c3b23d6516fea9d4
chrispiccaro <chrispiccaro18@gmail.com>
2020-08-23 13:53:30 -0700

    basic hooks on splash

--- src/components/Pages/Splash.jsx
+++ src/components/Pages/Splash.jsx
@@ -1,7 +1,43 @@
 import React from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
+
+const LoginButton = () => {
+  const { loginWithRedirect } = useAuth0();
+
+  return <button onClick={() => loginWithRedirect()}>Log In</button>;
+};
+
+const LogoutButton = () => {
+  const { logout } = useAuth0();
+
+  return <button onClick={() => logout({ returnTo: window.location.origin })}>
+    Log Out
+  </button>;
+};
+
+const Profile = () => {
+  const { user, isAuthenticated } = useAuth0();
+
+  return (
+    isAuthenticated && (
+      <div>
+        <img src={user.picture} alt={user.name} />
+        <h2>{user.name}</h2>
+        <p>{user.email}</p>
+      </div>
+    )
+  );
+};
 
 const Splash = () => {
-  return <h1>Splash Page</h1>;
+  return (
+    <>
+      <h1>Splash Page</h1>
+      <LoginButton/>
+      <LogoutButton/>
+      <Profile/>
+    </>
+  );
 };
 
 export default Splash;
