6cf3a286edc54690a1671445afd8e3e6ac54e51f
Eve Stockstill <evestockstillpdx@gmail.com>
2020-08-30 19:38:56 -0700

    add some font awsome and worked on nav function

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import {
+  faTimes,
+  faBars,
+} from '@fortawesome/free-solid-svg-icons';
+library.add(fab, faTimes, faBars);
 // import Splash from '../Pages/Splash';
 import NavBar from '../Nav/NavBar';
 
 export default function App() {
   return (
+
     <Router>
-      <NavBar />;
+      <NavBar />
     </Router>
   );
 }
--- src/components/Nav/NavBar.jsx
+++ src/components/Nav/NavBar.jsx
@@ -3,24 +3,32 @@ import { Link } from 'react-router-dom';
 import ButtonNav from './ButtonNav.css';
 import Dropdown from './Dropdown';
 import './NavBar.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const NavBar = () => {
-  const [open, setOpen] = useState(false);
+  const x = (
+    <FontAwesomeIcon
+      icon='times'
+      className='menu-icon'
+    ></FontAwesomeIcon>
+  );
+  const bars = (
+    <FontAwesomeIcon
+      icon='bars'
+      className='menu-icon'
+    ></FontAwesomeIcon>
+  );
+  const [click, setClick] = useState(false);
 
-  const handleClick = () => setOpen(!open);
+  const handleClick = () => setClick(!click);
   return (
     <>
       <nav className='navbar'>
         <Link to='/' className='navbar-logo'>
           Diversify Portland
         </Link>
-        <div className='menu-icon' onClick={handleClick}>
-          <svg viewBox='0 0 100 80' width='40' height='40'>
-            <rect width='100' height='20' rx='8'></rect>
-            <rect y='30' width='100' height='20' rx='8'></rect>
-            <rect y='60' width='100' height='20' rx='8'></rect>
-          </svg>
-          {/* <i className={open ? 'fas fa-times' : 'fas fa-bars'} /> */}
+        <div onClick={handleClick} className='menu-icon'>
+          <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
         </div>
       </nav>
     </>
